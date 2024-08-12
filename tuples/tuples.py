5c4abdd5d2d28a6aeb82e9d98d85bdf0ccc0fcdc
Krishna Mandava <mandava.krishna777@gmail.com>
2024-08-12 20:12:30 +0530

    Tupples

---
+++ tuples/tuples.py
@@ -0,0 +1,19 @@
+my_fruits = ("Apple", "Banana")
+
+print(my_fruits[0])
+
+# my_fruits[0] ="Anjeers"
+
+(x,*y)=my_fruits
+
+print(x,y)
+
+print(type(my_fruits))
+
+print(len(my_fruits))
+
+if "Apple" in my_fruits:
+    print("Exist")
+else:
+    print("Not Exist")
+
